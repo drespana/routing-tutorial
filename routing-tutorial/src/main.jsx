e649373fb173576bf60e5c2c145e75b54abc687a
drespana <drespana@google.com>
2024-04-08 11:02:55 -0500

    react routing tutorial since I did routing with Angular in the last porject

---
+++ routing-tutorial/src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
